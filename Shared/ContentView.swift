c066adf8c8b2850543cbfd9b2bceabed3db7c6a8
Mitch Tabian <mitch@tabian.ca>
2021-03-15 15:23:04 -0700

    Capturing user input from Textfield

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
+    @State private var name = ""
     var body: some View {
-        Button("Tap Count \(tapCount)"){
-            self.tapCount += 1
+        Form{
+            TextField(
+                "Enter your name",
+                text: $name
+            )
         }
     }
 }
