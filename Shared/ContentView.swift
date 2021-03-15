ca4ede922d112bf15e3cadfe4c6da0c30a2eea66
Mitch Tabian <mitch@tabian.ca>
2021-03-15 15:30:51 -0700

    Using a picker to select an option

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    var students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
     var body: some View {
-        Form{
-            TextField(
-                "Enter your name",
-                text: $name
-            )
-            Text("Your name is \(name)")
+        Picker("Select your student", selection: $selectedStudent){
+            ForEach(0 ..< students.count){
+                Text(self.students[$0])
+            }
         }
     }
 }
