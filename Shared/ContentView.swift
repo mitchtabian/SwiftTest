c0e769a08a81fba190b5015d8b21026fdb47ed2c
Mitch Tabian <mitch@tabian.ca>
2021-03-16 15:04:51 -0700

    a look at generics

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,29 @@
 
 import SwiftUI
 
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack{
+            ForEach(0 ..< rows){ row in
+                HStack{
+                    ForEach(0 ..< columns){column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("hello")
+        GridStack(rows: 4, columns: 4){row, col in
+            Text("R\(row) C\(col)")
+        }
     }
 }
 
