a7c3c5a8afbdea11dc266680798152e7dede8ca1
Mitch Tabian <mitch@tabian.ca>
2021-03-16 14:23:23 -0700

    custom modifiers

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View{
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(
+            alignment: .bottomTrailing,
+            content: {
+                content
+                Text(text)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color.black)
+            }
+        )
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+extension View {
+    func watermarked(text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("hello")
+        Color.blue
+            .frame(width: 300, height: 300)
+            .watermarked(text: "Hacking with swift")
+        Text("hello").titleStyle()
     }
 }
 
