d446fcea69b732e353821c3dcd5debc610dec482
Mitch Tabian <mitch@tabian.ca>
2021-03-17 14:26:26 -0700

    Observable classes

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    
+    @ObservedObject private var user = User()
+    
     var body: some View {
-        Text("hello")
+        VStack{
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
     }
 }
 
