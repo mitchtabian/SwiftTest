e643da3dae7e74cdae616179be331fc139877e5c
Mitch Tabian <mitch@tabian.ca>
2021-03-17 13:08:42 -0700

    rotation animation demo

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
     var body: some View {
-        Text("hello")
+        Button("tap me"){
+            withAnimation{
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0.0, y: 1.0, z: 0.0)
+        )
     }
 }
 
