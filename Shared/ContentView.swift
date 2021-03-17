5ce42b90e2c013278e1f4cebffb3783d71b4b203
Mitch Tabian <mitch@tabian.ca>
2021-03-17 14:32:23 -0700

    showing and hiding a bottom sheet

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,28 @@
 
 import SwiftUI
 
+struct SecondView: View{
+    
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
+    
+    var body: some View{
+        Button("Dismiss"){
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var showingSheet = false
+    
     var body: some View {
-        Text("hello")
+        Button("Show sheet"){
+            showingSheet.toggle()
+        }.sheet(isPresented: $showingSheet, content: {
+            SecondView(name: "@mitch_tabian")
+        })
     }
 }
 
