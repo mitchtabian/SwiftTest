e4459f273c644efc729ff7e993bd5e60b2a75f87
Mitch Tabian <mitch@tabian.ca>
2021-03-17 13:49:38 -0700

    custom animation transition modifier

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+
+struct CornerRotateModifier: ViewModifier{
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View{
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition{
+    static var pivot: AnyTransition{
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     
     @State private var isShowingRed = false
@@ -23,8 +39,7 @@ struct ContentView: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
-                    .transition(.scale)
-//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
             }
             
         }
