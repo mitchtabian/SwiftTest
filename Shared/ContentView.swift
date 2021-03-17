e74451189d35bdcafb03db4d7f3151d795c3efbb
Mitch Tabian <mitch@tabian.ca>
2021-03-17 13:27:58 -0700

    Drag gestures demo

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("hello")
+        LinearGradient(
+            gradient: Gradient(colors: [.yellow, .red]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+        .frame(width: 300, height: 200)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .offset(dragAmount)
+        .gesture(
+            DragGesture().onChanged {
+                dragAmount = $0.translation
+            }
+            .onEnded{ _ in
+                withAnimation(.spring()){
+                    dragAmount = .zero
+                }
+            }
+        )
     }
 }
 
