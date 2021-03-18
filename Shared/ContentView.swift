b37fa473c271b4555a98d3766e5fb6b118e39bc3
Mitch Tabian <mitch@tabian.ca>
2021-03-18 11:24:57 -0700

    decoding some json with Codable

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("hello")
+        Button("Decode json"){
+            let input = """
+                {
+                    "name": "Taylor Swift",
+                    "address": {
+                        "street": "555, Taylor Swift Avenue",
+                        "city": "Nashville"
+                    }
+                }
+                """
+            struct User: Codable{
+                var name: String
+                var address: Address
+            }
+            
+            struct Address: Codable{
+                var street: String
+                var city: String
+            }
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from: data){
+                print(user.address.street)
+            }
+        }
     }
 }
 
