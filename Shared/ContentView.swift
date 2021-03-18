8c46375f883feae2195d9b2922c4890053a6b2a4
Mitch Tabian <mitch@tabian.ca>
2021-03-18 15:18:05 -0700

    binding state

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+struct PushButton: View{
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white:0.6), Color(white: 0.4)]
+    
+    var body: some View{
+        Button(title){
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5 )
+    }
+}
 struct ContentView: View {
+    
+    @State private var rememberMe = false
+    
     var body: some View {
-        Text("hello")
+        VStack{
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
