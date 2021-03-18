8a258c40f53b48548be9efcd27b75540b843806c
Mitch Tabian <mitch@tabian.ca>
2021-03-18 13:43:50 -0700

    example of a network request example to iTunes api

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,51 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    
+    @State var results = [Result]()
+    
     var body: some View {
-        Text("hello")
+        List(results, id: \.trackId){item in
+            VStack(alignment: .leading){
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData(){
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+                print("Invalid URL")
+                return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request){ data, response, error in
+            if let data = data{
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
+                    DispatchQueue.main.async{
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unkown error")")
+        }.resume()
     }
 }
 
