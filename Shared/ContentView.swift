095ababffefb419de348163965bb3b945311a084
Mitch Tabian <mitch@tabian.ca>
2021-03-18 13:06:28 -0700

    drawing a circle and triangle

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,41 @@
 
 import SwiftUI
 
+struct Triangle: Shape{
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+    
+        return path
+    }
+}
+
+struct Arc: Shape{
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockWise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockWise)
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("hello")
+        Arc(startAngle: .degrees(0), endAngle: .degrees(180), clockWise: false)
+            .stroke(Color.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+        
+//        Triangle()
+//            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .frame(width: 300, height: 300)
     }
 }
 
