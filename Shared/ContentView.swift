96d049cb03a932e9569056e1aa309cc7c5282578
Mitch Tabian <mitch@tabian.ca>
2021-03-22 11:55:54 -0700

    network result wrapper

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
+
 struct ContentView: View {
     var body: some View {
         Text("hello")
+            .onAppear {
+                self.fetchData(
+                    from: "https://www.apple.com",
+                    completion: { result in
+                        switch result {
+                        case .success(let str):
+                            print(str)
+                        case .failure(let error):
+                            switch error{
+                            case .badURL:
+                                print("BAD URL")
+                            case .requestFailed:
+                                print("Network problems")
+                            case .unknown:
+                                print("Unknown error")
+                            }
+                        }
+                    }
+                )
+            }
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping(Result<String, NetworkError>) -> Void)  {
+        guard let url = URL(string: urlString) else{
+            completion(.failure(.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url){ data, response, error in
+            DispatchQueue.main.async{
+                if let data = data{
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                }else if error != nil{
+                    completion(.failure(.requestFailed))
+                }else{
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
     }
 }
 
