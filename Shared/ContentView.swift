565a0ec909a9a383c3f114bd132858ee022940ec
Mitch Tabian <mitch@tabian.ca>
2021-03-22 12:02:49 -0700

    will set example for published object

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,9 +7,31 @@
 
 import SwiftUI
 
+class DelayedUpdater: ObservableObject{
+    
+//    @Published var value = 0
+    
+    var value = 0 {
+        willSet{
+            objectWillChange.send()
+        }
+    }
+    
+    init(){
+        for i in 1...10{
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i), execute: {
+                self.value += 1
+            })
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var updater = DelayedUpdater()
+    
     var body: some View {
-        Text("hello")
+        Text("Value is: \(updater.value)")
     }
 }
 
